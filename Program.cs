5033ba4df3a01eb7c3f9cdd303a0341391a986bd
Khalmatov <large000@yandex.ru>
2021-10-07 22:36:40 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Module_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (string Name,string[] Dishes) User;
+
+            Console.WriteLine("Введите имя пользователя:");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            for(int i = User.Dishes.Length - 1;i >= 0;i--)
+            {
+                Console.WriteLine("Введите любимое блюдо №{0}",i+1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
         }
     }
 }
