0d6b6bf3e082793580fb9fa0fb5343221b4593b5
Khalmatov <large000@yandex.ru>
2021-10-09 21:10:40 +0300

    Задание 5.2.3

--- Program.cs
+++ Program.cs
@@ -7,8 +7,32 @@ namespace Module_5
         static void Main(string[] args)
         {
 
-            var favcolors = new string[3];
-            for(int i = 0;i < favcolors.Length;i++)
+            (string myName, byte age, DayOfWeek day, string birthdate) anketa;
+
+            Console.WriteLine("What is your name?");
+            anketa.myName = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            bool isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
+            while (!isAgeCorrenct)
+            {
+                Console.WriteLine("Enter your age");
+                isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
+            }
+            Console.WriteLine("What is your favourite day of week?");
+            anketa.day = (DayOfWeek)checked(byte.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter your birthdate: ");
+            anketa.birthdate = checked(Console.ReadLine());
+            Console.WriteLine("My name is {0}", anketa.myName);
+            Console.WriteLine("My age is {0}", anketa.age);
+            Console.WriteLine("Your favourite day of week is " + anketa.day);
+            Console.WriteLine("Your birthday is " + anketa.birthdate);
+
+
+
+
+            var array = GetArrayFromConsole();
+            int i = 1, j = 2;
+            while (i < array.Length)
             {
                 favcolors[i] = ShowColor();
             }
