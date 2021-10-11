d0743b136443421afa3c7220b517d818434819f4
Khalmatov <large000@yandex.ru>
2021-10-11 22:49:25 +0300

    Задание 5.3.1

--- Program.cs
+++ Program.cs
@@ -7,43 +7,50 @@ namespace Module_5
         static void Main(string[] args)
         {
 
-            (string myName, byte age, DayOfWeek day, string birthdate) anketa;
+            //(string myName, byte age, DayOfWeek day, string birthdate) anketa;
 
-            Console.WriteLine("What is your name?");
-            anketa.myName = Console.ReadLine();
-            Console.WriteLine("Enter your age");
-            bool isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
-            while (!isAgeCorrenct)
-            {
-                Console.WriteLine("Enter your age");
-                isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
-            }
-            Console.WriteLine("What is your favourite day of week?");
-            anketa.day = (DayOfWeek)checked(byte.Parse(Console.ReadLine()));
-            Console.WriteLine("Enter your birthdate: ");
-            anketa.birthdate = checked(Console.ReadLine());
-            Console.WriteLine("My name is {0}", anketa.myName);
-            Console.WriteLine("My age is {0}", anketa.age);
-            Console.WriteLine("Your favourite day of week is " + anketa.day);
-            Console.WriteLine("Your birthday is " + anketa.birthdate);
+            //Console.WriteLine("What is your name?");
+            //anketa.myName = Console.ReadLine();
+            //Console.WriteLine("Enter your age");
+            //bool isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
+            //while (!isAgeCorrenct)
+            //{
+            //    Console.WriteLine("Enter your age");
+            //    isAgeCorrenct = byte.TryParse(Console.ReadLine(), out anketa.age);
+            //}
+            //Console.WriteLine("What is your favourite day of week?");
+            //anketa.day = (DayOfWeek)checked(byte.Parse(Console.ReadLine()));
+            //Console.WriteLine("Enter your birthdate: ");
+            //anketa.birthdate = checked(Console.ReadLine());
+            //Console.WriteLine("My name is {0}", anketa.myName);
+            //Console.WriteLine("My age is {0}", anketa.age);
+            //Console.WriteLine("Your favourite day of week is " + anketa.day);
+            //Console.WriteLine("Your birthday is " + anketa.birthdate);
 
             
 
 
-            var favcolors = new string[3];
+            //var favcolors = new string[3];
 
-            ShowArray(GetArrayFromConsole(10),true);
-            //var sortedarray = SortArray(GetArrayFromConsole(3));
+            //ShowArray(GetArrayFromConsole(10),true);
+            ////var sortedarray = SortArray(GetArrayFromConsole(3));
 
-            for (int i = 0;i < favcolors.Length;i++)
-            {
-                favcolors[i] = ChangeColor(anketa.myName,anketa.age);
-            }
-            //ShowColors(anketa.myName, favcolors[0],favcolors[2]);
-            ShowColors();
+            //for (int i = 0;i < favcolors.Length;i++)
+            //{
+            //    favcolors[i] = ChangeColor(anketa.myName,anketa.age);
+            //}
+            ////ShowColors(anketa.myName, favcolors[0],favcolors[2]);
+            //ShowColors();
 
+            string test_name = "Vasya";
+            Console.WriteLine(test_name);
+            GetName(test_name); //  работа по значению.
+            Console.WriteLine(test_name);
 
-           
+            int age = 20;
+            Console.WriteLine(age);
+            ChangeAge(age); //  работа по значению.
+            Console.WriteLine(age);
 
         }
 
@@ -122,6 +129,19 @@ namespace Module_5
             if (switchSort) result = SortArray(array);
             else result = array;
             foreach (var arrItem in result) Console.Write(arrItem + " ");
+        }
+
+        static void GetName(string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.WriteLine("Введите возраст:");
+            age = Convert.ToInt32(Console.ReadLine());
         }
 
     }
